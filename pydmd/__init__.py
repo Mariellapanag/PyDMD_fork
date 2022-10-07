5d1defdae013e38485c746e67f8462797939151f
Mariella <mariella1990998@gmail.com>
2022-10-07 16:52:19 +0100

    Added new variables for new classes X,Y Sample_DMD, Sample_mrDMD

--- pydmd/__init__.py
+++ pydmd/__init__.py
@@ -19,3 +19,5 @@ from .spdmd import SpDMD
 from .paramdmd import ParametricDMD
 from .dmd_modes_tuner import ModesTuner
 from .subspacedmd import SubspaceDMD
+from .sample_xy_dmd_mrdmd import SampleXY_DMD, SampleXY_MrDMD
+from .sample_dmd_mrdmd import Sample_DMD, Sample_MrDMD
